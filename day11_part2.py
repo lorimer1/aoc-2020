e09ced9cf183ba6b5b5fc92088600bfa618f9980
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-13 11:15:17 +1300

    very inefficient but works (50 mins!)

--- day11_part2.py
+++ day11_part2.py
@@ -16,7 +16,19 @@ r, c = 0, 1  # index of x and y axis in a point
 
 def get_angle(seat1, seat2): 
     return math.atan2(seat2[c] - seat1[c], seat1[r] - seat2[r]) % (2 * math.pi)
-            
+
+def get_test_angles():
+    test_angles = []
+    test_angles.append(get_angle((0,0),(-1,0))) # 0 deg
+    test_angles.append(get_angle((0,0),(-1,1))) # 45 deg
+    test_angles.append(get_angle((0,0),(0,1))) # 90 deg
+    test_angles.append(get_angle((0,0),(1,1))) # 135 deg
+    test_angles.append(get_angle((0,0),(1,0))) # 180 deg
+    test_angles.append(get_angle((0,0),(1,-1))) # 225 deg
+    test_angles.append(get_angle((0,0),(0,-1))) # 270 deg
+    test_angles.append(get_angle((0,0),(-1,-1))) # 315 deg
+    return test_angles
+
 def get_seats(grid):
     seats = []
     for row in range(len(grid)):
@@ -25,38 +37,52 @@ def get_seats(grid):
                 seats.append((row,col))
     return seats
 
-def seat_test(grid, test_seat):
+def seat_test(grid, test_seat, test_angles):
+    tr, tc = test_seat
     seats = get_seats(grid)
-
     if test_seat in seats: seats.remove(test_seat)
-
+    seats_on_angles = []
+    for seat in seats:
+        if get_angle(test_seat, seat) in test_angles:
+            seats_on_angles.append(seat)
+    
     # sort each seat by it's length from our test_seat, sort seats by distance from our test_seat
-    seats.sort(key=lambda seat: math.hypot(seat[c] - test_seat[c], seat[r] - test_seat[r]))
+    seats_on_angles.sort(key=lambda seat: math.hypot(seat[c] - test_seat[c], seat[r] - test_seat[r]))
 
     # rank each seat based on the number of seats on the same angle. 0 for closest, 1 for next closest
     ranks = {seat: sum(get_angle(test_seat, seat) == get_angle(test_seat, other_seat)
-                for other_seat in seats[:i]) for i, seat in enumerate(seats)}
+                for other_seat in seats_on_angles[:i]) for i, seat in enumerate(seats_on_angles)}
     
     # keep only closest
     visible_seats = {key:val for key, val in ranks.items() if val == 0}
+    taken_seats_count = 0
+    for key in visible_seats.keys():
+        if grid[key[r]][key[c]]=="#":
+            taken_seats_count += 1
 
-    # # sort by closet asteroids on each angle ... get 199th asteroid in this list
-    # # x1, y1 = sorted(asteroids, key=lambda asteroid: (
-    # #     ranks[asteroid], angle(our_base, asteroid)))[199]
+    if grid[tr][tc] == "L":
+        if taken_seats_count == 0:
+            return "#"
+        else:
+            return "L"
 
-    return None
+    if grid[tr][tc] == "#":
+        if taken_seats_count >= 5:
+            return "L"
+        else:
+            return "#"
+    return "."
 
 @aoc_timer()
 def solve(input):
+    test_angles = get_test_angles()
     grid_current = []
     grid_next = [list(line) for line in input.splitlines()]
-    iterations = 0
     while grid_next!=grid_current:
-        iterations+=1
         grid_current = deepcopy(grid_next)
         for row in range(len(grid_current)):
             for col in range(len(grid_current[0])):
-                grid_next[row][col] = seat_test(grid_current, (row, col))
+                grid_next[row][col] = seat_test(grid_current, (row, col), test_angles)
     return sum(s.count('#') for s in grid_current)
 
 if __name__ == '__main__':
