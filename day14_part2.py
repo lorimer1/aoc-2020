4bf11332537e6c4c48bc515f55009c885e8528d6
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-14 19:54:15 +1300

    renamed vars for improved readability

--- day14_part2.py
+++ day14_part2.py
@@ -8,27 +8,26 @@ import re
 def get_addresses(pattern, addresses):
     if 'X' not in pattern:
         return addresses
+    new_addresses = []
     for address in addresses:
-        new_addresses = []
-        for address in addresses:
-            new_addresses.append(address.replace('X', '1', 1))
-            new_addresses.append(address.replace('X', '0', 1))
-        return get_addresses(pattern.replace('X', '0', 1),new_addresses)
+        new_addresses.append(address.replace('X', '1', 1))
+        new_addresses.append(address.replace('X', '0', 1))
+    return get_addresses(pattern.replace('X', '0', 1),new_addresses)
 
-def mask_vals( val, mask ):
-    binary = bin(val).replace('0b','').zfill(len(mask))
-    masked = ''
-    for i in range(len(binary)):
+def mask_vals(address, mask):
+    address_binary = bin(address).replace('0b','').zfill(len(mask))
+    address_masked = ''
+    for i in range(len(address_binary)):
         if mask[i] == '0':
-            masked = masked + binary[i]
+            address_masked = address_masked + address_binary[i]
         else:
-            masked = masked + mask[i]
+            address_masked = address_masked + mask[i]
+    address_masked_as_list = []
+    address_masked_as_list.append(address_masked)
+    addresses_binary = get_addresses(address_masked, address_masked_as_list)
     addresses = []
-    addresses.append(masked)
-    binary_addresses = get_addresses(masked, addresses)
-    addresses = []
-    for binary_address in binary_addresses:
-        addresses.append(int(binary_address, 2))
+    for address_binary in addresses_binary:
+        addresses.append(int(address_binary, 2))
     return addresses
 
 @aoc_timer()
@@ -37,16 +36,16 @@ def solve(input):
     program = {}
     lines = input.splitlines()
     for line in lines:
-        left, right = line.split(' = ')
-        if left == 'mask':
-            mask = right
+        left_of_equal, right_of_equal = line.split(' = ')
+        if left_of_equal == 'mask':
+            mask = right_of_equal
             continue
-        matches = re.search('^mem\[(?P<index>\d+)\]$', left)
-        index = int(matches.group('index'))
-        value = int(right)
-        addresses = mask_vals(index, mask)
+        matches = re.search('^mem\[(?P<mem_val>\d+)\]$', left_of_equal) #extract mem address as mem_val
+        address_from_input = int(matches.group('mem_val'))
+        value_from_input = int(right_of_equal)
+        addresses = mask_vals(address_from_input, mask)
         for address in addresses:
-            program[address] = value
+            program[address] = value_from_input
 
     return sum( program.values() )
 
