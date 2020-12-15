9d75e6530c5e6c66d33a34d87e7a1f83ec1e0c79
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-15 17:11:24 +1300

    file created from template in preparation for day

---
+++ day09_part1.py
@@ -0,0 +1,13 @@
+# Advent of code
+# Author = Rob Lorimer
+YEAR=2020; DAY=9; PART=1
+
+from aoc_utils import aoc_read_input, aoc_timer
+
+@aoc_timer()
+def solve(input):
+    return "todo"
+
+if __name__ == '__main__':
+    puzzle_input = aoc_read_input(YEAR, DAY)
+    print(f'Part {PART}: ', solve(puzzle_input))
---
+++ day15_part2_test.py
@@ -0,0 +1,13 @@
+#replace 'template' with 'dayn'
+import unittest
+from aoc_utils import aoc_read_input
+from day15_part2 import YEAR, DAY, solve
+
+class aoc_test(unittest.TestCase):
+
+    def test(self):
+        test_input = aoc_read_input(YEAR, DAY, extra='_test')
+        self.assertEqual(solve(test_input), "todo")
+
+if __name__ == '__main__':
+    unittest.main()
