103c4f7b9aba8726dbf8d2a289019d4fae5dff03
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-15 19:05:48 +1300

    Day 15 Complete

--- day15_part1.py
+++ day15_part1.py
@@ -6,7 +6,16 @@ from aoc_utils import aoc_read_input, aoc_timer
 
 @aoc_timer()
 def solve(input):
-    return "todo"
+    spoken = list(map(int,input.split(',')))
+    latest = spoken[-1]
+    spoken = list(reversed(spoken[:-1]))
+    for turn in range(len(spoken)+1,2020):
+        new_num = 0
+        if latest in spoken:
+            new_num = spoken.index(latest) + 1
+        spoken.insert(0,latest)
+        latest = new_num
+    return latest
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
--- day15_part2.py
+++ day15_part2.py
@@ -6,7 +6,16 @@ from aoc_utils import aoc_read_input, aoc_timer
 
 @aoc_timer()
 def solve(input):
-    return "todo"
+    spoken = list(map(int,input.split(',')))
+    latest = spoken[-1]
+    positions = {k:v+1 for v, k in enumerate(spoken[:-1])}
+    for turn in range(len(spoken),30000000):
+        new_num = 0
+        if latest in positions:
+            new_num = turn - positions[latest]
+        positions[latest] = turn
+        latest = new_num
+    return latest
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
