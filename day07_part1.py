21023186fa39f5535e8aa591b9d3a7d24643c205
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-17 21:13:11 +1300

    Day 17 Complete

--- day07_part1.py
+++ day07_part1.py
@@ -6,6 +6,7 @@ from aoc_utils import aoc_read_input, aoc_timer
 from functools import lru_cache
 
 bags = {}
+bags_outer={}
 
 def make_bags_collection(input):
     input = input.replace(' bags', '').replace(' bag', '').replace(' contain ','|').replace('.','').replace(', ',',').splitlines()
@@ -20,6 +21,7 @@ def make_bags_collection(input):
                 parts = child.split(' ',1) 
                 child_bags[parts[1]] = int(parts[0]) #[1]=bag, [0]=quantity
         bags[parent_txt] = child_bags
+        bags_outer[parent_txt]=False
     return bags
 
 @lru_cache(maxsize=600)
@@ -28,16 +30,24 @@ def is_suitable_bag(test_bag, our_bag):
     if not test_bag_children:
         return False
     if our_bag in test_bag_children.keys():
+        bags_outer[test_bag]=True
         return True
     for child_bag in test_bag_children.keys():
-        return is_suitable_bag(child_bag, our_bag)
+        if is_suitable_bag(child_bag, our_bag):
+            bags_outer[child_bag]=True
+            return True
+        return False
 
 @aoc_timer()
 def solve(input):
     make_bags_collection(input)
+    l = len(bags)
     count = 0
     for bag in bags.keys():
-        count += 1 if is_suitable_bag(bag, 'shiny gold') else 0
+        if bag == 'shiny gold':
+            continue
+        if is_suitable_bag(bag, 'shiny gold'):
+            count += 1 
     return count
 
 if __name__ == '__main__':
--- day17_part2.py
+++ day17_part2.py
@@ -3,10 +3,61 @@
 YEAR=2020; DAY=17; PART=2
 
 from aoc_utils import aoc_read_input, aoc_timer
+import copy
+
+ACTIVE, INACTIVE = '#','.'
+CYCLES = 6
+OFFSETS = (-1,0,1)
 
 @aoc_timer()
 def solve(input):
-    return "todo"
+    grid = {}
+
+    # initialise 2D  grid at z=0
+    z,w=0,0
+    for y, line in enumerate(input.splitlines()):
+        for x, c in enumerate(line):
+            grid[(x,y,z,w)] = c
+
+    for cycle in range(CYCLES):
+        # add new layer of empty units 
+        # i.e. extend the full grid by 1 unit in all directions
+        grid_copy = copy.deepcopy(grid)
+        for k in grid_copy.keys(): # use a copy for iteration vars so changes to grid don't cause side-effects
+            x,y,z,w = k
+            for dx in OFFSETS:
+                for dy in OFFSETS:
+                    for dz in OFFSETS:
+                        for dw in OFFSETS:
+                            xt=x+dx; yt=y+dy; zt=z+dz; wt=w+dw
+                            test_k=(xt,yt,zt,wt)
+                            if test_k not in grid:
+                                grid[test_k]=INACTIVE
+        
+        # create a new gid from the old; make changes to that so we don't get side affects during iteration of orginal
+        grid_next = copy.deepcopy(grid)
+        for k,v in grid.items():
+            active_count=0
+            x,y,z,w = k
+            for dx in OFFSETS:
+                for dy in OFFSETS:
+                    for dz in OFFSETS:
+                        for dw in OFFSETS:
+                            xt=x+dx; yt=y+dy; zt=z+dz; wt=w+dw
+                            test_k=(xt,yt,zt,wt)
+                            test_v=grid.get(test_k, INACTIVE)
+                            if k==test_k: #skip testing our unit
+                                continue
+                            if test_v==ACTIVE:
+                                active_count+=1
+            if v==ACTIVE and not active_count in (2,3):
+                grid_next[k]=INACTIVE
+            if v==INACTIVE and active_count==3:
+                grid_next[k]=ACTIVE
+
+        grid=copy.deepcopy(grid_next)
+
+    return len([v for v in grid.values() if v==ACTIVE])  
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
