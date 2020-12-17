774df83eabd11292201bc5dbc87224896e7b28f3
Rob Lorimer <rob.lorimer@gmail.com>
2020-12-17 21:46:46 +1300

    Day 7 complete

--- day07_part1.py
+++ day07_part1.py
@@ -4,51 +4,30 @@ YEAR=2020; DAY=7; PART=1
 
 from aoc_utils import aoc_read_input, aoc_timer
 from functools import lru_cache
+import re
 
 bags = {}
-bags_outer={}
 
-def make_bags_collection(input):
-    input = input.replace(' bags', '').replace(' bag', '').replace(' contain ','|').replace('.','').replace(', ',',').splitlines()
-    for line in input:
-        parent_txt, children_txt = line.split('|')
-        children = children_txt.split(',')
-        child_bags = {}
-        for child in children:
-            if child == 'no other':
-                break
-            else:
-                parts = child.split(' ',1) 
-                child_bags[parts[1]] = int(parts[0]) #[1]=bag, [0]=quantity
-        bags[parent_txt] = child_bags
-        bags_outer[parent_txt]=False
-    return bags
-
-@lru_cache(maxsize=600)
-def is_suitable_bag(test_bag, our_bag):
-    test_bag_children = bags[test_bag]
-    if not test_bag_children:
-        return False
-    if our_bag in test_bag_children.keys():
-        bags_outer[test_bag]=True
+@lru_cache(600)
+def has_shiny_gold(colour):
+    if colour == "shiny gold": 
         return True
-    for child_bag in test_bag_children.keys():
-        if is_suitable_bag(child_bag, our_bag):
-            bags_outer[child_bag]=True
-            return True
-        return False
+    else:
+        return any(has_shiny_gold(c) for _, c in bags[colour] )
 
 @aoc_timer()
 def solve(input):
-    make_bags_collection(input)
-    l = len(bags)
-    count = 0
-    for bag in bags.keys():
-        if bag == 'shiny gold':
-            continue
-        if is_suitable_bag(bag, 'shiny gold'):
-            count += 1 
-    return count
+    bag_count = 0
+
+    for line in input.splitlines():
+        colour = re.match(r"(.+?) bags contain", line)[1]  
+        bags[colour] = re.findall(r"(\d+?) (.+?) bags?", line)
+
+    for bag in bags:
+        if has_shiny_gold(bag):
+            bag_count += 1
+
+    return bag_count - 1
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
--- day07_part2.py
+++ day07_part2.py
@@ -3,10 +3,24 @@
 YEAR=2020; DAY=7; PART=2
 
 from aoc_utils import aoc_read_input, aoc_timer
+from functools import lru_cache
+import re
+
+bags = {}
+
+@lru_cache(600)
+def count_bags(bag_type):
+    return 1 + sum(int(number)*count_bags(colour) for number, colour in bags[bag_type])
 
 @aoc_timer()
 def solve(input):
-    return "todo"
+    bag_count = 0
+
+    for line in input.splitlines():
+        colour = re.match(r"(.+?) bags contain", line)[1]  
+        bags[colour] = re.findall(r"(\d+?) (.+?) bags?", line)
+
+    return count_bags("shiny gold")-1
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
