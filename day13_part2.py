b05ec0129e616050d3805e7789f4964b583a36bc
lorimer1 <rob.lorimer@fonterra.com>
2020-12-17 16:53:32 +1300

    DAy 13 Done - lot of optimising for/while loop!

--- day13_part2.py
+++ day13_part2.py
@@ -7,13 +7,13 @@ from aoc_utils import aoc_read_input, aoc_timer
 @aoc_timer()
 def solve(input):
     arrival, timetable = input.splitlines()
-    bus_wait_times = {}
-    for route in timetable.split(','):
-        if not route == 'x':
-            bus = int(route)
-            bus_wait_times[bus] = (bus * (int(arrival)//bus) + bus) - int(arrival)
-    best_bus = min(bus_wait_times, key=bus_wait_times.get)
-    return best_bus * bus_wait_times[best_bus]
+    bus_t_offset = {int(k):i for i,k in enumerate(timetable.split(',')) if k != 'x'}
+    t, step = 0, 1
+    for bus, offset in bus_t_offset.items():
+        while (t+offset) % bus!=0:
+            t+=step
+        step *= bus
+    return t
 
 if __name__ == '__main__':
     puzzle_input = aoc_read_input(YEAR, DAY)
--- day13_part2_test.py
+++ day13_part2_test.py
@@ -7,7 +7,23 @@ class aoc_test(unittest.TestCase):
 
     def test(self):
         test_input = aoc_read_input(YEAR, DAY, extra='_test')
-        self.assertEqual(solve(test_input), 295)
+        self.assertEqual(solve(test_input), 1068781)
+
+    def test1(self):
+        test_input = '4444\n67,7,59,61'
+        self.assertEqual(solve(test_input), 754018)
+
+    def test2(self):
+        test_input = '4444\n67,x,7,59,61'
+        self.assertEqual(solve(test_input), 779210)
+
+    def test3(self):
+        test_input = '4444\n67,7,x,59,61'
+        self.assertEqual(solve(test_input), 1261476)
+
+    def test4(self):
+        test_input = '4444\n1789,37,47,1889'
+        self.assertEqual(solve(test_input), 1202161486)
 
 if __name__ == '__main__':
     unittest.main()
