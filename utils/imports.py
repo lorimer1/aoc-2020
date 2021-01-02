8b10c8a4d6675cd99bd9bb5f37e89380533e9e15
Rob Lorimer <rob.lorimer@gmail.com>
2021-01-03 12:12:58 +1300

    initial commit

---
+++ utils/imports.py
@@ -0,0 +1,14 @@
+# add project directory to path
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# combinations(numbers, 2)
+# combinations(numbers, 3) 
+from itertools import combinations 
+
+# count(0, 3)
+from itertools import count
+
+# re_policy = re.compile(r'(\d+)-(\d+) (\w): (\w+)')
+# policies = re_policy.findall(input)
+import re
