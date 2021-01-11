ee3bbbac0e86ebee88271eae66f3483c0e3b7267
lorimer1 <rob.lorimer@fonterra.com>
2021-01-11 18:03:24 +1300

    functools partial, reduce

--- utils/imports.py
+++ utils/imports.py
@@ -2,6 +2,13 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+#into_sets = partial(map, set)
+from functools import partial
+
+
+from functools import reduce
+
+
 # combinations(numbers, 2)
 # combinations(numbers, 3) 
 from itertools import combinations 
@@ -12,3 +19,4 @@ from itertools import count
 # re_policy = re.compile(r'(\d+)-(\d+) (\w): (\w+)')
 # policies = re_policy.findall(input)
 import re
+
